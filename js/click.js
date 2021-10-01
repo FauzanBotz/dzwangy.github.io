8ed63ce3bafb0f67b0ef518b06b7e661057b5993
FauzanBot <87386162+FauzanBotz@users.noreply.github.com>
2021-10-01 17:14:52 +0700

    Update click.js

--- js/click.js
+++ js/click.js
@@ -1,5 +1,5 @@
 function openLink() {
 
-    window.open('https://api.whatsapp.com/send?phone=6283862247572&text=6567', '_blank')
+    window.open('https://api.whatsapp.com/send?phone=6289516004292&text=2879', '_blank')
 
 }
